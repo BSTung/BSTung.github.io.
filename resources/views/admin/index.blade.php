1253fa18b4a904bfef65cb6144e2cef8af2f5130
TungBS <buisontung1997@gmail.com>
2020-07-20 15:56:42 +0700

    adding my files to my repository

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app_master_admin')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>Trang quản trị hệ thống website bán thực phẩm bổ sung</h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><!-- <i class="fa fa-dashboard"></i> Home</a></li> -->
+        </ol>
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Thông tin sinh viên</h3>
+                <h5>
+                    <p>Tên sinh viên: Bùi Sơn Tùng</p>
+                    <p>Lớp          : K04-Cntt3</p>
+                    <p>MSSV         : 0450080147</p>
+                </h5>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+                    <i class="fa fa-minus"></i></button>
+                    <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                        <i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+                <div class="box-body">
+                    
+                </div>
+
+            </div>
+            <!-- /.box -->
+    </section>
+    <!-- /.content -->
+@stop
